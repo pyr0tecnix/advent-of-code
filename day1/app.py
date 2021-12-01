1274b8d79903c74ebd33f8dbc70579259b2d6eef
pyr0tecnix <contact@vieyra.fr>
2021-12-01 22:45:54 +0100

    Day 1 ok

--- day1/app.py
+++ day1/app.py
@@ -1,3 +1,23 @@
 # --*-- encoding: utf-8 --*--
 
 
+count = 0
+
+
+file = open("data/input.txt")
+data = file.readlines()
+file.close()
+
+
+for (index, value) in enumerate(data):
+    if (index > 0) and (index < len(data)):
+        current, previous = value, data[index - 1]
+        # print(previous, current, current > previous)
+        if(int(current) >= int(previous)):
+            count +=1
+
+print(count)
+
+
+
+
