22c4ab468c8c7a38d80d2c29972401a92296f40e
pyr0tecnix <contact@vieyra.fr>
2021-12-01 23:14:11 +0100

    Day 1 bonus

---
+++ day1/bonus.py
@@ -0,0 +1,29 @@
+# --*-- encoding: utf-8 --*--
+
+
+count = 0
+
+
+file = open("data/input.txt")
+data = file.readlines()
+file.close()
+
+windowed = []
+
+for (index, value) in enumerate(data):
+    if (index > 1) and (index < len(data)):
+        current, previous, antePrevious = value, data[index - 1], data[index - 2]
+        print(antePrevious, previous,current, int(current) + int(previous) + int(antePrevious))
+        windowed.append(int(current) + int(previous) + int(antePrevious))
+    # if(index > 20):
+    #     break
+
+
+for (index, value) in enumerate(windowed):
+    if (index < len(data)):
+        current, previous = value, windowed[index - 1]
+        print(previous, current, current > previous)
+        if(int(current) >= int(previous)):
+            count +=1
+
+print(count)
