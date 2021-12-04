9ef316a4c6237774c552306ca1b8bdffd27816ab
Pyrotecnix <contact@vieyra.fr>
2021-12-04 14:56:13 +0100

    Get list and grids ok

--- src/day4/app.py
+++ src/day4/app.py
@@ -11,9 +11,23 @@ file.close()
 
 # Bingo numbers
 bingoNumbers = data[0]
+del data[0]
 
+# Grids
+grids = []
+
+singleGrid = []
+# Bingo grid
 for key, value in enumerate(data):
-    print(value)
+    # print(key, value)
+    if key % 6 != 0:
+        singleGrid.append(value.replace("\n",""))
+    else:
+        print(singleGrid)
+        grids.append(singleGrid)
+        singleGrid = []
 
+
+# print(grids)
 
 
