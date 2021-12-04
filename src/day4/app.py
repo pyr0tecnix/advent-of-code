6249664939d8ec41e16dac13d3b9256e93e22cf8
Pyrotecnix <contact@vieyra.fr>
2021-12-04 15:21:49 +0100

    Create some utilities

--- src/day4/app.py
+++ src/day4/app.py
@@ -1,9 +1,5 @@
 # --*-- encoding: utf-8 --*--
 
-
-from os import major
-
-
 file = open("data/sample.txt", "r")
 data = file.readlines()
 file.close()
@@ -16,18 +12,38 @@ del data[0]
 # Grids
 grids = []
 
+def getRow(grid, row):
+    return grid[row]
+
+def getColumn(grid, column):
+    return [col[column] for col in grid]
+
+
+def getHammingWeight(vector):
+    weight = 0
+    for key, value in enumerate(vector):
+        if list(value.values())[0]:
+            weight += 1
+    return weight
+
 singleGrid = []
 # Bingo grid
 for key, value in enumerate(data):
     # print(key, value)
     if key % 6 != 0:
-        singleGrid.append(value.replace("\n",""))
+        value = value.replace("\n","")
+        valueList = []
+        for val in value.split(" "):
+            valueList.append({val:0})
+        singleGrid.append(valueList)
     else:
-        print(singleGrid)
-        grids.append(singleGrid)
-        singleGrid = []
+        # print(singleGrid)
+        if singleGrid:
+            grids.append(singleGrid)
+            singleGrid = []
 
 
-# print(grids)
-
+print(getRow(grids[0], 1))
+print(getColumn(grids[0], 1))
+print(getHammingWeight(getRow(grids[0], 1)))
 
