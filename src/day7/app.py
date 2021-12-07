514a32f4f3a7724e47277f777eb723fbe1d466e3
Pyrotecnix <contact@vieyra.fr>
2021-12-07 20:09:31 +0100

    Day7 ok

--- src/day7/app.py
+++ src/day7/app.py
@@ -1,14 +1,27 @@
+def moveToPosition(crabs, position):
+    crabLine = []
+    for key, crab in enumerate(crabs):
+        crabLine.append(abs(crab - position))
+
+    return sum(crabLine)
+
+
 def crabAlign():
-    with open('data/sample.txt') as file:
+    with open('data/input.txt') as file:
         [initialPosition] = file.readlines()
         initialPosition = list(map(int, initialPosition.split(",")))
 
         minP = min(initialPosition)
         maxP = max(initialPosition)
-        
         print("Bounds : {}/{}".format(minP, maxP))
-        for position in initialPosition:
-            print(position)
+        # printMap(hypothesis)
+
+        positions = []
+        for i in range(maxP):
+           positions.append(moveToPosition(initialPosition, i))
+        #    print(positions)
+
+        print("Minimum value {}".format(min(positions)))
 
 
 crabAlign()
