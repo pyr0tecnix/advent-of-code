00eff3769e581a677b1d0ebed1ff91dfd59be0ac
Pyrotecnix <contact@vieyra.fr>
2021-12-08 19:39:38 +0100

    Day8 ok

--- src/day8/app.py
+++ src/day8/app.py
@@ -1,16 +1,17 @@
-def moveToPosition(crabs, position):
-    crabLine = []
-    for key, crab in enumerate(crabs):
-        crabLine.append(abs(crab - position))
-
-    return sum(crabLine)
 
 
 def decode7segment():
-    with open('data/sample.txt') as file:
+    with open('data/input.txt') as file:
         data = file.readlines()
-        signals, output = data.split("|")
-        print(output)
+        signals = [line.split("|")[0].strip() for line in data]
+        outputs = [line.split("|")[1].strip() for line in data]
 
+    # By obvious I mean 1, 4, 7 and 8
+    totalObvious = 0
+    for output in outputs:
+        outList = output.split(" ")
+        print(outList)
+        totalObvious+= sum(map(lambda x: len(x) == 2, outList)) + sum(map(lambda x: len(x) == 3, outList)) + sum(map(lambda x: len(x) == 4, outList)) + sum(map(lambda x: len(x) == 7, outList))
 
+    print(totalObvious)
 decode7segment()
