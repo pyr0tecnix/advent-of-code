8b7b6a10d9fe6d328516fbde5bc80657cb953a44
Pyrotecnix <contact@vieyra.fr>
2021-12-15 21:36:27 +0100

    Day10 on going

--- src/day10/app.py
+++ src/day10/app.py
@@ -1,20 +1,56 @@
 
 legalPairs = {'{': '}', '}': '{', '(' : ')', ')' : '(', '<' : '>', '>': '<', '[' : ']', ']' : '['}
 
+closing = {'}':'{', ')':'(', '>':'<', ']':'['}
+opening = {'{':'}', '(':')', '<':'>', '[':']'}
+
+def rindex(lst, value):
+    caractList = lst
+    # print("Rindex list : {}".format(len(lst)))
+    caractList.reverse()
+    i = caractList.index(value)
+    caractList.reverse()
+    return len(caractList) - i - 1
+
+def delete_multiple_element(list_object, indices):
+    indices = sorted(indices, reverse=True)
+    for idx in indices:
+        if idx < len(list_object):
+            list_object.pop(idx)
+
+
 def isIncomplete(chunk):
     return len(chunk) % 2 != 0
 
 def isCorupted(chunk):
-    delimiter = int(len(chunk) / 2)
-    print(delimiter)
-    firstHalf = chunk[:delimiter]
-    secondHalf = chunk[delimiter:]
-    print("{} is {} and {}".format(chunk, firstHalf, secondHalf))
+    for key, caracter in enumerate(chunk):
+        if caracter in closing.keys():
+            # Find matching opening
+            lookingFor = closing[caracter]
+            # print(caracter, lookingFor, len(chunk))
+            # print("{} - {} - {}".format(key, closing[caracter], chunk))
+            try:
+                firstMatch = rindex(chunk[0:key], lookingFor)
+                # print(lookingFor, firstMatch, chunk)
+                delete_multiple_element(chunk, [key, firstMatch])
+                # print(chunk)
+                isCorupted(chunk)
+            except (ValueError):
+                # print(chunk, caracter)
+                print("VE : Expected {}, but found {} instead.".format(opening[chunk[0]], caracter))
+                chunk.clear()
+
+    if chunk:
+        # print(chunk)    
+        print("Expected {}, but found {} instead.".format(opening[chunk[0]], opening[chunk[-1]]))
+        chunk.clear()
+
+
 
 def readChunks():
     with open('data/sample.txt') as file:
-        chunks = [line.strip() for line in file]
-        
+        chunks = [list(line.strip()) for line in file]
+
 
     for chunk in chunks:
         if not isIncomplete(chunk):
@@ -22,7 +58,4 @@ def readChunks():
 
 
 
-
 readChunks()
-
-print(legalPairs['('])
