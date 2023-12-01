55d70bdcc2bd6c0ef28feece23de980a9cb32fa5
pyr0tecnix <contact@vieyra.fr>
2023-12-01 22:56:30 +0100

    Day one ok

---
+++ src/2023/day1/app.py
@@ -0,0 +1,21 @@
+import re
+
+
+def readFile(filename):
+    file = open("data/{}".format(filename), "r")
+    rawData = file.read().splitlines()
+    file.close()
+    return rawData
+
+data = readFile("input.txt")
+
+calibration = []
+for line in data:
+    numerals = re.findall(r"[0-9]", line)
+    if len(numerals) == 1:
+        cal = [numerals[0] + numerals[0]]
+    else:
+        cal = [numerals[0] + numerals[-1]]
+    # string to int
+    calibration.append(int(cal[0]))
+print(sum(calibration))
