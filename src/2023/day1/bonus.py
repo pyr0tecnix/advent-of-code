75b9d1930fb226c228365b884914b41aecae17c3
pyr0tecnix <contact@vieyra.fr>
2023-12-02 13:26:10 +0100

    Solve with help

--- src/2023/day1/bonus.py
+++ src/2023/day1/bonus.py
@@ -1,63 +1,30 @@
 import re
 
-
+#Thanks : https://www.reddit.com/r/adventofcode/comments/1883ibu/2023_day_1_solutions/
 def readFile(filename):
     file = open("data/{}".format(filename), "r")
     rawData = file.read().splitlines()
     file.close()
     return rawData
 
-data = readFile("sample_bonus.txt")
+data = readFile("input.txt")
 
-litterals = [('one', '1'), ('two', '2'), ('three', '3'), ('four', '4'), ('five', '5'), ('six', '6'), ('seven', '7'), ('eight', '8'), ('nine', '9')]
+litterals = [('one', 'o1e'), ('two', 't2o'), ('three', 'e3e'), ('four', 'e4e'), ('five', 'e5e'), ('six', 'e6e'), ('seven', '7e'), ('eight', 'e8e'), ('nine', 'e9e')]
 
-def recursive_replace(s):
-    if(s.isnumeric()):
-        return s
-    index = next(i for i, j in enumerate(s) if j not in ['1','2','3','4','5','6','7','8','9'])
-    # print(s, index, s[index:], s[index:].startswith('nine'))
-    if index >=0 :
-        if s[index:].startswith('one'):
-            s = s.replace('one', '1', 1)
-            recursive_replace(s)
-        if s[index:].startswith('two'):
-            s = s.replace('two', '2', 1)
-            recursive_replace(s)
-        if s[index:].startswith('three'):
-            s = s.replace('three', '3', 1)
-            recursive_replace(s)
-        if s[index:].startswith('four'):
-            s = s.replace('four', '4', 1)
-            recursive_replace(s)
-        if s[index:].startswith('five'):
-            s = s.replace('five', '5', 1)
-            recursive_replace(s)
-        if s[index:].startswith('six'):
-            s = s.replace('six', '6', 1)
-            recursive_replace(s)
-        if s[index:].startswith('seven'):
-            s = s.replace('seven', '7', 1)
-            recursive_replace(s)
-        if s[index:].startswith('eight'):
-            s = s.replace('eight', '8', 1)
-            recursive_replace(s)
-        if s[index:].startswith('nine'):
-            s = s.replace('nine', '9', 1)
-            recursive_replace(s)
+def word_to_number(s):
+    for k, v in litterals:
+        s=s.replace(k,v)
     return s
+
 
 calibration = []
 for line in data:
-    # Transcrypt litteral numbers
-    # for key, value in litterals:
-    #     line = line.replace(key, value)
-    s = recursive_replace(recursive_replace(line))
-    print(s)
-#     numerals = re.findall(r"[0-9]", line)
-#     if len(numerals) == 1:
-#         cal = [numerals[0] + numerals[0]]
-#     else:
-#         cal = [numerals[0] + numerals[-1]]
-#     # string to int
-#     calibration.append(int(cal[0]))
-# print((calibration))
+    line = word_to_number(line)
+    numerals = re.findall(r"[0-9]", line)
+    if len(numerals) == 1:
+        cal = [numerals[0] + numerals[0]]
+    else:
+        cal = [numerals[0] + numerals[-1]]
+    # string to int
+    calibration.append(int(cal[0]))
+print(sum(calibration))
