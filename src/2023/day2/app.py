dfbab1d0e2e32afa5a66f6e82048c25cac5d970f
pyr0tecnix <contact@vieyra.fr>
2023-12-02 14:32:07 +0100

    Day 2 ok

---
+++ src/2023/day2/app.py
@@ -0,0 +1,64 @@
+import re
+
+
+def readFile(filename):
+    file = open("data/{}".format(filename), "r")
+    rawData = file.read().splitlines()
+    file.close()
+    return rawData
+
+
+def returnNumber(value):
+    # print(value, int(re.findall("[0-9]+", value)[0]))
+    return int(re.findall("[0-9]+", value)[0])
+
+def getGameId(line):
+    return returnNumber(line[0:line.find(':')])
+
+# Return an list of 3 values r,g,b
+def getTirageData(line):
+    #remove Game X
+    red = []
+    green = []
+    blue = []
+
+    line = line[line.find(':') + 1:]
+    tirages = line.split(';')
+    # print(line)
+    for tirage in tirages:
+        #Split by color
+        colorSplit = tirage.split(',')
+        r = 0
+        g = 0
+        b = 0
+        for color in colorSplit:
+            if 'red' in color:
+                r = returnNumber(color)
+            if 'green' in color:
+                g = returnNumber(color)
+            if 'blue' in color:
+                b = returnNumber(color)
+        # print(r,g,b)
+        red.append(r)
+        green.append(g)
+        blue.append(b)
+        # print(red, green, blue)
+    return [max(red), max(green), max(blue)]
+
+def isGamePossible(redCount, greenCount, blueCount):
+    games = 0
+    for line in data:
+        game = getGameId(line)
+        tirage = getTirageData(line)
+        if tirage[0] <= redCount and tirage[1] <= greenCount and tirage[2] <= blueCount:
+            games += game
+    return games
+
+
+data = readFile("input.txt")
+
+# print(getTirageData(data[4]))
+
+print(isGamePossible(12, 13, 14))
+
+
